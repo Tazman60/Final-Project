354a162a6674b92afefe8e04dd0646ed94bbef0e
Tazman60 <mlorenzo@msn.com>
2026-05-29 07:10:26 -0400

    Still Working

--- index.js
+++ index.js
@@ -2,20 +2,32 @@
 
 const movieListEl = document.querySelector(".movie-list");
 
-async function main() {
-    const movies = await fetch("http://www.omdbapi.com/?apikey=c1438081&t=Guardians+of+the+Galaxy");
-    const movieData = await movies.json();
-    movieListEl.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("")
+var api = 'https://www.omdbapi.com/?apikey=c1438081&s=';
+var input = 'guardians';
+
+function setup() {
+    var button = select('#submit');
+    button.mousePressed(main);
+    input = select('#movieKeyword');
 }
 
-main();
+//var url = api + input.value();
 
-function movieHTML(movie) {
+async function main() {
+    
+    const movies = await fetch("https://www.omdbapi.com/?apikey=c1438081&s=" + input);
+    const movieData = await movies.json();
+    movieListEl.innerHTML = movieData.Search.map((movie) => movieHTML(movie)).join("");
+ }
+
+ main();
+
+ function movieHTML(movie) {
     return `<div class="movie-card">
         <div class="movie-card__container">
             <h3>Title: ${movie.Title}</h3>
                 <p><b>Year:</b> ${movie.Year}</p>
-                <p><b>Poster:</b> ${movie.Poster}</p>
+                <p><b>Poster:</b> <a href="${movie.Poster}" target="_blank">${movie.Poster}</a></p>
             </div>
         </div>`
-}
+    }
